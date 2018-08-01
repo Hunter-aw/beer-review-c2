461ef54f51a79bed98d44c5c27ed56fb6f89e020
hunter-aw <hunteraweinsheink@gmail.com>
2018-08-01 10:19:40 +0300

    finished the project, added correct data flow and bootstrap formatting

---
+++ script.js
@@ -0,0 +1,37 @@
+const COUNTRIES = []
+let MYLIST = $('.country-list')
+ 
+
+function addCountry (country, review){
+    let newEntry = {
+        myCountry: country,
+        myReview: review
+    }
+    COUNTRIES.push(newEntry)
+}
+
+function renderCountries () {
+    MYLIST.empty();
+    for (entry of COUNTRIES) {
+        MYLIST.append('<l1 class = "list-group-item fa fa-globe-americas">' + entry.myCountry + ': </li><br><span class ="font-weight-light">' + entry.myReview + '</span><br>')
+    }
+}
+
+const clickPost = function() {
+    let countryName = $('.country-input').val();
+    let reviewName = $('.review-input').val();
+    addCountry(countryName, reviewName)
+    renderCountries()
+}
+
+
+$('.post-country').click(clickPost);
+
+
+$(document).ready(function(){
+    $('.country-form').keypress(function(e){
+      if(e.keyCode==13)
+      $('.post-country').click();
+    });
+});
+
